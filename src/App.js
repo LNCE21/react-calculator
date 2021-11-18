afa511852e1c3bc877c0d4624d32117200189f3d
LNCE21 <lautaronicolascc@gmail.com>
2021-11-18 17:43:20 -0300

    Added a reducer to manage all the states (current and previous operands and the operations. Also added a global variable of actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./styles.css"
 
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate"
+}
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || }`
+      }
+  }
+}
+
 function App() {
+  const [currentOperand, previousOperand, operation] = useReducer(reducer, {})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
